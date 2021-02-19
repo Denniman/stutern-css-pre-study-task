bfe032e9942588bb8ea8d81bbe3625c797b7e238
Dennis <dennimanadams@gmail.com>
2021-02-20 00:22:04 +0100

    add note functionality

--- js/app.js
+++ js/app.js
@@ -2,49 +2,61 @@ const button = document.querySelector('.btn')
 const modal = document.querySelector('.modal')
 const modalContent = document.querySelector('#modal--cont')
 const closeBtn = document.querySelector('.close')
+const form = document.querySelector('.modal__form')
+const noteTitle = document.querySelector('.input__title')
+const noteBody = document.querySelector('.textarea')
 
-const notes = [ 
-    {
-        title: 'Spatulas',
-        body: 'Yesterday I went to the store and got some much-needed'
-    },
-    {
-        title: 'Cookie Monster Cupcakes',
-        body: 'My favorite cartoon character is Cookie Monster'
-    }
-]
+const notes = []
 
 const notesSummary = () => {
     const notesContainer = document.querySelector('.notes-summary')
     notesContainer.innerHTML = ''
     const summary = document.createElement('h2')
-    summary.textContent = `You have ${notes.length} notes`
+    summary.textContent = `You have ${notes.length + 2} notes`
     notesContainer.appendChild(summary)
 }
 
 notesSummary()
 
-// render notes to screen
-// step 1: create a new [article] element
-// step 2: add textContent to the new element
-document.createElement('article').classList.add('article')
-
 
 button.addEventListener('click', () => {
-    // const titleInput = prompt('Enter note title')
-    // const bodyInput = prompt('Write your note', 'empty note')
-
-    // if(titleInput.length > 0 && bodyInput.length > 0) {
-    //     notes.push({
-    //         title: prompt('Enter note title'),
-    //         body: prompt('Write your note', 'empty note')
-    //     })
-    // }
-    
     modal.style.display = "block"
-    modalContent.classList.add('visible')
-    console.log(notes)
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    notes.push({
+        title: noteTitle.value,
+        body: noteBody.value
+    })
+
+    const date = new Date();
+    const articleContainer = document.querySelector('.article__container')
+    const article = document.createElement('article')
+    article.classList.add('article')
+    articleContainer.appendChild(article)
+    // note title
+    const titleText = document.createElement('h2')
+    // titleText.textContent = `${moment().format('DD/MM/YYYY')}: ${noteTitle.value}`
+    titleText.textContent = `${date.toLocaleDateString("en-GB")}: ${noteTitle.value}`
+    article.appendChild(titleText)
+
+    // note body
+    const articleBody = document.createElement('div')
+    articleBody.classList.add('article__content')
+    article.appendChild(articleBody)
+    const bodyText = document.createElement('p')
+    bodyText.textContent = noteBody.value
+    articleBody.appendChild(bodyText)
+
+
+
+    noteTitle.value = ''
+    noteBody.value = ''
     notesSummary()
+    modal.style.display = 'none'
+    // console.log(notes)
 })
 
 closeBtn.addEventListener('click', () => {
